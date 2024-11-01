d0d5fe294325e6d7587485e554557b51b142ed25
kenkimatsuda1115 <always1115ken@gmail.com>
2024-11-01 22:48:29 +0900

    Add backend folder

---
+++ backend/src/infrastructure/database/mongoose.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const mongoUri = '<TODO: MONGO_URI>';
+
+export async function connectDB() {
+  try {
+    await mongoose.connect(mongoUri);
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+}
